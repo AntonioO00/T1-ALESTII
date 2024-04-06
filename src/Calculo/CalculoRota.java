e223124ff22cee133e7d32b6705aa80b122d0f01
victor <vitormachado2001@gmail.com>
2024-04-06 15:46:02 -0300

    Commit do boolean e do jogo (perdi)

--- src/Calculo/CalculoRota.java
+++ src/Calculo/CalculoRota.java
@@ -23,18 +23,28 @@ public class CalculoRota {
     }
 
     public void LeituraMatriz(){
+
         boolean esquerda = false;
         boolean cima = false;
         boolean baixo = false;
         boolean direita = true;
+
         for(i = coordenada(); tetris[i][j]=='#'; i++){
             for(j = 0; tetris[i][j]=='#'; j++){
-                if(direita && tetris[i][j] == '\\'){
+
+               if(baixo){MudaBaixo(i, j);}
+               if(cima){MudaCima(i, j);}
+               if(esquerda){MudaEsquerda(i, j);}
+               if(direita){MudaDireita(i, j);}
+
+               if(direita && tetris[i][j] == '\\'){
                     baixo = true;
                     direita = false;
                     cima = false;
                     esquerda= false;
-                    MudaBaixo(i,j);}
+                    MudaBaixo(i,j);
+                }
+
                 if(direita && tetris[i][j] == '/'){
                     baixo = false;
                     direita = false;
